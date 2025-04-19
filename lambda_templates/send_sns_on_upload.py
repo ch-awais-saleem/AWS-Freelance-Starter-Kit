5140e2ac2c066d557cb3d00a1c69e2c879a893d2
Muhammad Awais Saleem <115088021+ch-awais-saleem@users.noreply.github.com>
2025-04-20 01:11:09 +0500

    Update send_sns_on_upload.py

--- lambda_templates/send_sns_on_upload.py
+++ lambda_templates/send_sns_on_upload.py
@@ -1,1 +1,17 @@
+import boto3
 
+def lambda_handler(event, context):
+    sns = boto3.client('sns')
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+
+    message = f"A new file was uploaded to {bucket}/{key}"
+    topic_arn = 'arn:aws:sns:your-region:your-account-id:your-topic-name'
+
+    sns.publish(
+        TopicArn=topic_arn,
+        Subject='New S3 Upload Notification',
+        Message=message
+    )
+    
+    return {'status': 'Notification sent'}
