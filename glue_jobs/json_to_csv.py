156e5fba688e1d86554859a1bcc8aa2f6c54f8e0
Muhammad Awais Saleem <115088021+ch-awais-saleem@users.noreply.github.com>
2025-04-20 01:10:26 +0500

    Update json_to_csv.py

--- glue_jobs/json_to_csv.py
+++ glue_jobs/json_to_csv.py
@@ -1,1 +1,31 @@
+# Folder: glue_jobs/json_to_csv.py
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'SOURCE_PATH', 'TARGET_PATH'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+source_path = args['SOURCE_PATH']
+target_path = args['TARGET_PATH']
+
+dyframe = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    connection_options={"paths": [source_path]},
+    format="json"
+)
+
+# Flatten if nested and convert to DataFrame
+df = dyframe.toDF()
+df.write.mode("overwrite").csv(target_path, header=True)
+
+job.commit()
 
