33f8b8f2fc37ed5660c4db9b87d45f5fd1ddeb41
Muhammad Awais Saleem <115088021+ch-awais-saleem@users.noreply.github.com>
2025-04-20 01:09:52 +0500

    Update s3_trigger_to_glue.py

--- lambda_templates/s3_trigger_to_glue.py
+++ lambda_templates/s3_trigger_to_glue.py
@@ -1,1 +1,20 @@
+import boto3
 
+def lambda_handler(event, context):
+    glue = boto3.client('glue')
+    
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    
+    glue.start_job_run(
+        JobName='json-to-csv-job',
+        Arguments={
+            '--SOURCE_PATH': f's3://{bucket}/{key}',
+            '--TARGET_PATH': f's3://your-target-bucket/converted/'
+        }
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': f'Started Glue job for file: {key}'
+    }
